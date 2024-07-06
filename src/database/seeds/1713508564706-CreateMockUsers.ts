7bca80f37817254b3dce071bb014050ace87ebbd
Barış Gündüzer <barisgunduzer@users.noreply.github.com>
2024-07-06 04:32:20 +0300

    mock user create bug fixed

--- src/database/seeds/1713508564706-CreateMockUsers.ts
+++ src/database/seeds/1713508564706-CreateMockUsers.ts
@@ -7,12 +7,11 @@ import { User } from '@api/entities/User';
 export class CreateMockUsers1713508564706 implements Seeder {
   public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
     // Seed from dataset
-    const users = userDataSet.map((data) => UserRepository.createOrFail(data));
-    await UserRepository.save(users);
+    userDataSet.map((data) => UserRepository.createOrFail(data));
 
     // Seed from factory
     const userFactory = factoryManager.get(User);
     // await userFactory.save();
-    await userFactory.saveMany(30);
+    await userFactory.saveMany(10);
   }
 }
